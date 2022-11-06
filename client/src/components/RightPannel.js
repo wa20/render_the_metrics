831ed511c70f11a267dcb0330b63f6f0efb8f69d
wael <78425538+wa20@users.noreply.github.com>
2022-11-06 18:00:06 +0000

    right pannel built and now pulls in metric data every 30secs using axios and useeffectand usestate

--- client/src/components/RightPannel.js
+++ client/src/components/RightPannel.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import "./style.css"
-
+import React from "react";
+import {useEffect, useState} from "react"
+import axios from "axios"
+import "./style.css";
 
 export default function RightPanel() {
-    return (
-    <> 
 
-        <div className=''>
-            <h2>Hello Right</h2>
-        </div>
+    const [state, setState] = useState([])
+
+
+    useEffect(() => { 
+      const interval = setInterval(() => { 
+
+        axios.get("http://localhost:3500/metrics")
+        .then((res) => {
+
+          const metrics = res.data;
+          setState(metrics)
+          this.setPosts({ metrics });
+          
+        }).catch((err) => {
+            console.log(err)
+        });
+
+      }, 30000)
+
+      return () => clearInterval(interval);
+    },[])
+
     
-
+  return (
+    <>
+      <div className="py-5">
+        <div className="black-board d-flex justify-content-center align-items-center">
+          <pre>{state}</pre>
+        </div>
+      </div>
     </>
- )
+  );
 }
